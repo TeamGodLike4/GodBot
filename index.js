0aa59776822ad408cd283568d0a55599ce2eaf83
TeamGodLike4 <alokhr04@outlook.com>
2024-06-09 13:06:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-//base by DGXeon
-//Om senn cuman recode
-
-const {
-   spawn
-} = require('child_process')
-const path = require('path')
+const { spawn } = require('child_process');
+const path = require('path');
 
 function start() {
-   let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
-   console.log([process.argv[0], ...args].join('\n'))
-   let p = spawn(process.argv[0], args, {
-         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-      })
-      .on('message', data => {
-         if (data == 'reset') {
-            console.log('Restarting Bot...')
-            p.kill()
-            start()
-            delete p
-         }
-      })
-      .on('exit', code => {
-         console.error('Exited with code:', code)
-         if (code == '.' || code == 1 || code == 0) start()
-      })
+    let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)];
+    console.log([process.argv[0], ...args].join('\n'));
+
+    let p = spawn(process.argv[0], args, {
+        stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+    });
+
+    p.on('message', data => {
+        if (data === 'reset') {
+            console.log('Restarting Bot...');
+            p.kill();
+            p.on('exit', () => {
+                start();
+            });
+        }
+    });
+
+    p.on('exit', code => {
+        console.error('Exited with code:', code);
+        if (code !== 0) {
+            start();
+        }
+    });
 }
-start()
+
+start();
